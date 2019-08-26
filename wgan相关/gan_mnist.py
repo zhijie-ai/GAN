0d6b827be00e8d529295b0ac61de888f4a2fdc1c
zhijie-ai <15311484394@189.cn>
2019-08-26 15:57:46 +0800

    GAN 实现方式

---
+++ wgan相关/gan_mnist.py
@@ -0,0 +1,15 @@
+#----------------------------------------------
+# -*- encoding=utf-8 -*-                      #
+# __author__:'xiaojie'                        #
+# CreateTime:                                 #
+#       2019/4/11 16:12                       #
+#                                             #
+#               天下风云出我辈，                 #
+#               一入江湖岁月催。                 #
+#               皇图霸业谈笑中，                 #
+#               不胜人生一场醉。                 #
+#----------------------------------------------
+# 这是看了<<令人拍案叫绝的Wasserstein GAN>>的后续关于gp文章的代码
+# https://zhuanlan.zhihu.com/p/25071913
+# https://www.zhihu.com/question/52602529/answer/158727900
+# 代码:https://github.com/igul222/improved_wgan_training
